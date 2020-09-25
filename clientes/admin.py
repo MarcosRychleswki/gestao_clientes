fe58e714029ea6fd3cd60bda76a6f8df0a2591c1
MarcosRychlewski <vinirychlewski@gmail.com>
2020-09-25 15:17:48 -0300

    'filtros'

--- clientes/admin.py
+++ clientes/admin.py
@@ -17,9 +17,38 @@ class PersonAdmin(admin.ModelAdmin):
     )
     #fields = (('first_name', 'last_name'), ('age', 'salary'), 'bio', 'photo')
     #exlude = ('bio') para excluir um campo
-    list_display = ('first_name', 'last_name', 'age', 'salary', 'bio', 'photo')
+    list_display = ('first_name', 'last_name', 'age', 'salary', 'bio', 'tem_foto')
+    list_filter =  ('age', 'salary')
 
+
+    def tem_foto(self, obj):
+        if obj.photo:
+            return 'Sim'
+        else:
+            return 'Nao'
+    short_description = 'Possui foto'
+
+
+
+class VendasAdmin(admin.ModelAdmin):
+    readonly_fields = ['valor']
+    raw_id_fields = ('pessoa', 'produtos')
+    list_filter = ('pessoa', 'desconto')
+    list_display = ('id', 'pessoa', 'get_total')
+    search_fields = ('id', 'pessoa__first_name',)
+
+    def total(self, obj):
+        return obj.get_total()
+
+    total.short_description = 'Total'
+
+
+
+
+class ProdutosAdmin(admin.ModelAdmin):
+    list_display = ('id', 'descricao', 'preco')
+    search_fields = ('id', 'descricao',)
 
 admin.site.register(Person, PersonAdmin)
-admin.site.register(Produto)
-admin.site.register(Vendas)
+admin.site.register(Produto, ProdutosAdmin)
+admin.site.register(Vendas, VendasAdmin)
--- clientes/models.py
+++ clientes/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.db.models.signals import m2m_changed
+from django.dispatch import receiver
 
 class Person(models.Model):
     objects = None
@@ -30,11 +32,26 @@ class Produto(models.Model):
 
 class Vendas(models.Model):
     numero = models.CharField(max_length=7)
-    valor = models.DecimalField(max_digits=5, decimal_places=2)
+    valor = models.DecimalField(max_digits=5, decimal_places=2, null=True, blank=True)
     desconto = models.DecimalField(max_digits=5, decimal_places=2)
     imposto = models.DecimalField(max_digits=5, decimal_places=2)
     pessoa = models.ForeignKey(Person, null=True, blank=True, on_delete=models.CASCADE)
     produtos = models.ManyToManyField(Produto, blank=True)
 
+    @property
+    def get_total(self):
+        tot = 0
+        for produto in self.produtos.all():
+            tot += produto.preco
+        return (tot - self.desconto) - self.imposto
+
     def __str__(self):
         return self.numero
+
+
+@receiver(m2m_changed, sender=Vendas.produtos.through)
+def update_vendas_total(sender, instance, **kwargs):
+    total= instance.get_total()
+    instance.valor = instance.get_total()
+    instance.save()
+    #Vendas.objects.filters(id=instance.id).update(total=total)
